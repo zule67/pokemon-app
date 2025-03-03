0a929e646037c8c60321f2434cc3413e18155dc2
zule67 <33101007+zule67@users.noreply.github.com>
2025-03-03 19:45:21 +0100

    Adding detail component

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pokemon } from '../../models/pokemon.model';
+import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule } from '@angular/common';
+
+// Import of Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-pokemon-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './pokemon-detail.component.html',
-  styleUrl: './pokemon-detail.component.scss'
+  styleUrls: ['./pokemon-detail.component.scss']
 })
-export class PokemonDetailComponent {
+export class PokemonDetailComponent implements OnInit {
 
+  public pokemon: Pokemon | null = null;
+  public errorMessage: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private pokemonService: PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    const name = this.route.snapshot.paramMap.get('name'); // Get the name from URL
+    if (name) {
+      this.pokemonService.getPokemonDetails(name).subscribe({
+        next: (data: Pokemon) => {
+          this.pokemon = data;
+        },
+        error: (error: any) => {
+          this.errorMessage = error.message;
+        }
+      });
+    }
+  }
+
+  // Go back
+  goBack(): void {
+    this.router.navigate(['/']);
+  }
 }
