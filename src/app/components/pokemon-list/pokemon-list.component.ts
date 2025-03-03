ae87454ef99cdf3c2845ee0383c695f8de956e41
zule67 <33101007+zule67@users.noreply.github.com>
2025-03-03 19:37:04 +0100

    Adding pokemon List

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pokemon } from '../../models/pokemon.model';
+import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// Import of Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule, // Need for ngModel
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './pokemon-list.component.html',
-  styleUrl: './pokemon-list.component.scss'
+  styleUrls: ['./pokemon-list.component.scss']
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit {
 
+  public pokemons: Pokemon[] = [];
+  public searchTerm: string = '';
+  public errorMessage: string = '';
+
+  constructor(private pokemonService: PokemonService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemonList().subscribe({
+      next: (data: Pokemon[]) => {
+        this.pokemons = data;
+      },
+      error: (error: any) => {
+        this.errorMessage = error.message;
+      }
+    });
+  }
+
+  // Navigate to the detail view of the selected Pokemon.
+  viewDetails(name: string): void {
+    this.router.navigate(['/pokemon', name]);
+  }
+
+  // Getter to filter the list of Pokemon by search term
+  get filteredPokemons(): Pokemon[] {
+    if (!this.searchTerm) {
+      return this.pokemons;
+    }
+    return this.pokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
